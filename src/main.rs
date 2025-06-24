969eda70e33cfffc0066cba3ae2daabd4d0526ba
Viibrant <11376396+Viibrant@users.noreply.github.com>
2025-06-24 00:37:42 +0000

    feat(api): add command-line interface

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,41 @@
+use clap::{Parser, Subcommand};
 use hnf::Client;
 
+#[derive(Parser)]
+#[command()]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Top {
+        #[arg(short, long, default_value_t = 10)]
+        number: usize,
+    },
+    New {
+        #[arg(short, long, default_value_t = 10)]
+        number: usize,
+    },
+}
+
 fn main() -> anyhow::Result<()> {
+    let args = Cli::parse();
     let client = Client::new();
-    let ids = client.fetch_top_ids()?;
-    println!("{:#?}", &ids[0..10]);
+
+    match args.command {
+        Commands::Top { number } => {
+            let ids = client.fetch_top_ids(number)?;
+            let count = std::cmp::min(ids.len(), number as usize);
+            println!("{:#?}", &ids[..count]);
+        }
+        Commands::New { number } => {
+            let ids = client.fetch_new_stories(number)?;
+            let count = std::cmp::min(ids.len(), number as usize);
+            println!("{:#?}", &ids[..count]);
+        }
+    }
+
     Ok(())
 }
